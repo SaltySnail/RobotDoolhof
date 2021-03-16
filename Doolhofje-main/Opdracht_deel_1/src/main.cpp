0e7515b2f934af47d0d3cf20a30c8cf302d9dedb
SaltySnail <51403141+SaltySnail@users.noreply.github.com>
2021-03-16 23:11:02 +0100

    Boem de enige die werkt

---
+++ Doolhofje-main/Opdracht_deel_1/src/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_thread.h>
+#include <SDL2/SDL_image.h>
+#include "Model/maze.h"
+#include "Model/init.h"
+#include "Model/robot.h"
+#include "Model/input.h"
+
+int main(void) {
+	srand(time(NULL));
+	SDL_bool quit;
+	model::init init;
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+	SDL_Point counter = {0, 0};
+	SDL_Point startMaze = {0, 0}, endMaze = {SCREEN_WIDTH/ROOM_SIZE, SCREEN_HEIGHT/ROOM_SIZE};
+	bool endGenerating = 0;
+	init.sdl(&window, &renderer);
+	model::maze maze;
+	model::robot robot;
+	model::input input;	
+	maze.initRooms();
+	maze.setStartEnd(startMaze, endMaze);
+	robot.init(renderer, &maze);
+	
+	while (!quit) {
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+		SDL_RenderClear(renderer);
+		quit = input.update();
+		if (!endGenerating) { 	
+			maze.generate(&counter, &endGenerating);
+			//printf("generate step \n\n");
+		}
+		else {
+			robot.update();
+		}
+		//printf("step \n\n");
+		maze.stupidlyCorrectBorders(renderer);
+		robot.draw(renderer);
+		maze.draw(renderer);	
+		SDL_RenderPresent(renderer);
+		if (endGenerating) {
+			SDL_Delay(7);
+		}
+		else {
+			SDL_Delay(3); //1440p & 20 pixels/room
+		}
+	}
+	SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+	SDL_Quit();
+	IMG_Quit();
+
+	return 0;	
+}
